0c12f3da7df81c6b64440fcb0931dea2ce36a802
khHJP <112843781+khHJP@users.noreply.github.com>
2022-12-20 08:35:46 +0900

    원격저장소작업1

--- src/com/sh/git/Main.java
+++ src/com/sh/git/Main.java
@@ -13,6 +13,8 @@ package com.sh.git;
 public class Main {
 	
 	public void master() {
+		System.out.println("master!!!!!!");
+		System.out.println("origin/master에서 작성함!!!!!!");
 		
 	}
 
@@ -23,6 +25,7 @@ public class Main {
 	}
 
 	public void feature() {
-		
+		System.out.println("maaaaaaaaaaaaaster!!!!!!");
+		System.out.println("feeeeeeeeeeature!!!!!!");
 	}
 }
